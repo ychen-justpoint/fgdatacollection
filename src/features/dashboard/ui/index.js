7e8773dd3432e979f4c235ae48e24180b8c9fa28
ychen-justpoint <young.chen@justpoint.ca>
2023-08-27 10:02:42 -0700

    Update index.js

--- src/features/dashboard/ui/index.js
+++ src/features/dashboard/ui/index.js
@@ -4,8 +4,8 @@ import {
   Card,
   Col,
   Row,
-  Typography
-  
+  Typography,
+  Spin
 } from "antd";
 
 import { Link } from 'react-router-dom';
@@ -13,32 +13,7 @@ import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../../../authConfig";
 import { default as CommonContext } from '../../../app/common-context';
 
-const count = [
-  {
-    title: "Batch",
-    total: "$53,000",
-    totalinerror: "+30%",
-    link:"/batch"
-  },
-  {
-    title: "Message",
-    total: "3,200",
-    totalinerror: "+20%",
-    link:"/message"
-  },
-  {
-    title: "Data",
-    total: "+1,200",
-    totalinerror: "-20%",
-    link:"/data"
-  },
-  {
-    title: "Activity",
-    total: "$13,200",
-    totalinerror: "10%",
-    link:"/activity"
-  },
-];
+
 
 export default function Index(props) {
 
@@ -65,7 +40,7 @@ export default function Index(props) {
   useEffect(() => {
     acquireAccessToken();
   }, []);
-  
+
   const { common, updateCommon } = useContext(CommonContext);
 
   common.module = 'Dashboard';
@@ -75,36 +50,39 @@ export default function Index(props) {
   updateCommon(common);
 
   return (
-    <Row gutter={[24, 0]}>
-      {repo.data.map((c, index) => (
-        <Col
-          key={index}
-          xs={24}
-          sm={24}
-          md={12}
-          lg={6}
-          xl={6}
-        >
-          <Link to={c.link}>
-            <Card bordered={false}>
-              <div>
-                <Row align="middle" gutter={[24, 0]}>
-                  <Col xs={18}>
-                    <span>{c.title}</span>
-                    <Title level={3} style={{color: "red"}}>
-                      {c.totalinerror}
-                    </Title>
-                    <Title level={3}>
-                      <small>{c.total + " total"} </small>
-                    </Title>
-                  </Col>
-                </Row>
-              </div>
-            </Card>
-          </Link>
-        </Col>
-      ))}
-    </Row>
+    <Spin spinning={repo.isbusy}>
+      <Row gutter={[24, 0]}>
+        {repo.data.map((c, index) => (
+          <Col
+            key={index}
+            xs={24}
+            sm={24}
+            md={12}
+            lg={6}
+            xl={6}
+          >
+            <Link to={c.link}>
+              <Card bordered={false}>
+                <div>
+                  <Row align="middle" gutter={[24, 0]}>
+                    <Col xs={18}>
+                      <span>{c.title}</span>
+                      <Title level={3} style={{ color: "red" }}>
+                        {c.totalinerror}
+                      </Title>
+                      <Title level={3}>
+                        <small>{c.total + " total"} </small>
+                      </Title>
+                    </Col>
+                  </Row>
+                </div>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Spin>
+
   )
 }
 
